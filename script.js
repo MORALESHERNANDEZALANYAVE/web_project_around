0422a848959761908b5ba00b49342629099d1486
Morales Hernandez Alan Yave <alan.moralez2525@gmail.com>
2025-06-03 20:01:53 -0600

    incorporacion de script

--- script.js
+++
@@ -1,3 +0,0 @@
-let profile = document.querySelector(".profile");
-let profileEditButton = profile.querySelector(".profile__edit-button");
-console.log(profileEditButton);
---
+++ script/script.js
@@ -0,0 +1,26 @@
+const butEdit = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const butClose = document.querySelector(".popup__close");
+const form = document.querySelector(".popup__form");
+const inName = document.querySelector(".profile__name");
+const inAbout = document.querySelector(".profile__job");
+const inpName = document.querySelector("#name");
+const inpAbout = document.querySelector("#abaut");
+
+function openEdit() {
+  inpName.value = inName.textContent;
+  inpAbout.value = inAbout.textContent;
+  popup.classList.toggle("popup__open");
+}
+
+butEdit.addEventListener("click", openEdit);
+butClose.addEventListener("click", openEdit);
+
+function saveChange(e) {
+  e.preventDefault();
+  inName.textContent = inpName.value;
+  inAbout.textContent = inpAbout.value;
+  openEdit();
+}
+
+form.addEventListener("submit", saveChange);
